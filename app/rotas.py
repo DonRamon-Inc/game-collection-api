787708ac7921cde8d0f2476e739f36ce83fed8f1
GiovanniPrado <giovanni.jprado@gmail.com>
2022-07-11 17:29:18 -0300

    ajeitando conforme review da pull request

--- app/rotas.py
+++ app/rotas.py
@@ -5,11 +5,6 @@ from .controllers import usuario_controller
 TODO: Melhorar essa forma de declarar as rotas, as funções declaradas dentro de `declarar_rotas` não são usadas diretamente.
 """
 def declarar_rotas(app):
-  @app.post("/usuarios")
+  @app.post("/cadastro")
   def cadastrar_usuario():
     return usuario_controller.criar_usuario(request)
-
-  @app.put("/usuarios/<id>")
-  def atualizar_usuario(id):
-    return usuario_controller.editar_usuario(request, id)
-
