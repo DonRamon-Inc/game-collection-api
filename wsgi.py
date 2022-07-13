f73622f4ca46e0ad97aa48ed391d4a7c9e5a7563
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-07-12 22:53:36 -0300

    dá o bind da porta do heroku no comando do gunicorn

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,5 @@ from app.app import iniciar_app
 dotenv.load_dotenv()
 app = iniciar_app()
 
-port = os.getenv('PORT', 5000)
-app.run(port=port)
+port = int(os.getenv('PORT', 5000))
+app.run(host="0.0.0.0", port=port)
