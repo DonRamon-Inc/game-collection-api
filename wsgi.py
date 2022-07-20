eb5a67583ba14cdb6e4e40d8d727f89a94b9630c
RamonRCM <costa.m.ramon@gmail.com>
2022-07-20 20:11:31 -0300

    Bugfix: Resolvido erro de confirmação email/senha REVISADO

--- wsgi.py
+++ wsgi.py
@@ -8,6 +8,4 @@ app = iniciar_app()
 
 port = int(os.getenv('PORT', 5000))
 if __name__ == '__main__':
-  dotenv.load_dotenv()
-  app = iniciar_app()
-  app.run()
+  app.run(host="0.0.0.0", port=port)
