5664efaeef336942072e389ddc089903bfe4b19e
Kleydson Guilherme <kleydsong@gmail..com>
2022-08-09 22:14:42 -0300

    Coleta do Steam ID

--- app/rotas.py
+++ app/rotas.py
@@ -4,3 +4,5 @@ def declarar_rotas(app):
   app.add_url_rule("/cadastro", methods=["POST"], view_func=usuario_controller.criar_usuario)
 
   app.add_url_rule("/login", methods=["POST"], view_func=usuario_controller.logar_usuario)
+
+  app.add_url_rule("/authSteam", methods=["POST"], view_func=usuario_controller.auth_steam)
