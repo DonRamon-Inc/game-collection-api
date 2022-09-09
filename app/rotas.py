00dcdee8b467eb99ffedf371b86ab9ae829fa202
GiovanniPrado <giovanni.jprado@gmail.com>
2022-09-08 22:43:55 -0300

    implementando pytest na rota esqueci-senha

--- app/rotas.py
+++ app/rotas.py
@@ -1,4 +1,8 @@
 from .controllers import usuario_controller
+from flask import request
+
+def validar_usuario():
+  return usuario_controller.validar_usuario(request)
 
 def declarar_rotas(app):
     app.add_url_rule("/cadastro", methods=["POST"], view_func=usuario_controller.criar_usuario)
