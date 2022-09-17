b9efdf6c79abcedff6dd93be459e8ff361e9aded
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-17 12:42:05 -0300

    refatora o codigo para remover a maior parte dos erros de lint

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,40 +1,41 @@
-from .db import db
 from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db import db
 from ..utils.logger import Logger
 
 logger = Logger("UsuarioModel")
 
 class Usuario(db.Model):
-  id = db.Column(db.Integer, primary_key = True)
-  nome = db.Column(db.String(80), nullable = False)
-  email = db.Column(db.String(100), nullable = False, unique = True)
-  senha = db.Column(db.String(255), nullable = False)
-  data_nascimento = db.Column(db.DateTime, nullable = False)
-  steam_id = db.Column(db.String(80), nullable = True)
-  token_esqueci_senha = db.Column(db.String(255), nullable = True)
-  token_valido_ate = db.Column(db.DateTime, nullable = True)
+    id = db.Column(db.Integer, primary_key = True)
+    nome = db.Column(db.String(80), nullable = False)
+    email = db.Column(db.String(100), nullable = False, unique = True)
+    senha = db.Column(db.String(255), nullable = False)
+    data_nascimento = db.Column(db.DateTime, nullable = False)
+    steam_id = db.Column(db.String(80), nullable = True)
+    token_esqueci_senha = db.Column(db.String(255), nullable = True)
+    token_valido_ate = db.Column(db.DateTime, nullable = True)
 
-  def __init__(self, nome, email, senha, data_nascimento, steam_id = None, token_esqueci_senha = None, token_valido_ate = None):
-    self.nome = nome.strip()
-    self.email = email
-    self.senha = generate_password_hash(senha)
-    self.data_nascimento = data_nascimento
-    self.steam_id = steam_id
-    self.token_esqueci_senha = token_esqueci_senha
-    self.token_valido_ate = token_valido_ate
+    def __init__(self, usuario_dicionario):
+        self.nome = usuario_dicionario.get("nome").strip()
+        self.email = usuario_dicionario.get("email")
+        self.senha = generate_password_hash(usuario_dicionario.get("senha"))
+        self.data_nascimento = usuario_dicionario.get("data_nascimento")
+        self.steam_id = usuario_dicionario.get("steam_id", None)
+        self.token_esqueci_senha = usuario_dicionario.get("token_esqueci_senha", None)
+        self.token_valido_ate = usuario_dicionario.get("token_valido_ate", None)
 
-  def verificar_senha(self, senha):
-    return check_password_hash(self.senha, senha)
+    def verificar_senha(self, senha):
+        return check_password_hash(self.senha, senha)
 
-  def salvar(self, senha = None):
-    if senha != None:
-      logger.info(f"Salvando a alteração de senha do usuário {self.email}")
-      self.senha = generate_password_hash(senha)
-      db.session.add(self)
-      db.session.commit()
-      logger.info(f"Senha do usuário {self.email} alterada com sucesso")
-    else:
-      logger.info(f"Salvando usuário {self.email}")
-      db.session.add(self)
-      db.session.commit()
-      logger.info(f"Usuário {self.email} salvo com sucesso")
+    def salvar(self, senha = None):
+        if senha is not None:
+            logger.info(f"Salvando a alteração de senha do usuário {self.email}")
+            self.senha = generate_password_hash(senha)
+            db.session.add(self)
+            db.session.commit()
+            logger.info(f"Senha do usuário {self.email} alterada com sucesso")
+        else:
+            logger.info(f"Salvando usuário {self.email}")
+            db.session.add(self)
+            db.session.commit()
+            logger.info(f"Usuário {self.email} salvo com sucesso")
--- app/rotas.py
+++ app/rotas.py
@@ -1,16 +1,24 @@
 from .controllers import usuario_controller
 
 def declarar_rotas(app):
-  app.add_url_rule("/cadastro", methods=["POST"], view_func=usuario_controller.criar_usuario)
+    app.add_url_rule("/cadastro", methods=["POST"], view_func=usuario_controller.criar_usuario)
 
-  app.add_url_rule("/login", methods=["POST"], view_func=usuario_controller.logar_usuario)
+    app.add_url_rule("/login", methods=["POST"], view_func=usuario_controller.logar_usuario)
 
-  app.add_url_rule("/authSteam", methods=["POST"], view_func=usuario_controller.auth_steam)
+    app.add_url_rule("/authSteam", methods=["POST"], view_func=usuario_controller.auth_steam)
 
-  app.add_url_rule("/authSteam", methods=["DELETE"], view_func=usuario_controller.auth_steam_delete)
+    app.add_url_rule(
+      "/authSteam", methods=["DELETE"], view_func=usuario_controller.auth_steam_delete
+    )
 
-  app.add_url_rule("/esqueci_senha", methods=["POST"], view_func=usuario_controller.validar_usuario)
+    app.add_url_rule(
+      "/esqueci_senha", methods=["POST"], view_func=usuario_controller.validar_usuario
+    )
 
-  app.add_url_rule("/atualizar_senha", methods=["POST"], view_func=usuario_controller.atualizar_senha)
+    app.add_url_rule(
+      "/atualizar_senha", methods=["POST"], view_func=usuario_controller.atualizar_senha
+    )
 
-  app.add_url_rule("/listar_jogos_steam", methods=["GET"], view_func=usuario_controller.listar_jogos_steam)
+    app.add_url_rule(
+      "/listar_jogos_steam", methods=["GET"], view_func=usuario_controller.listar_jogos_steam
+    )
