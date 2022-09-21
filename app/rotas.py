611e71490b812a973e686fd34fe505243d0de2c7
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-21 11:57:42 -0300

    adiciona a camada de services e separa as entidades de usuario e jogos

--- app/rotas.py
+++ app/rotas.py
@@ -1,34 +1,25 @@
-from flask import request
-from .controllers import usuario_controller
-
-def auth_steam():
-    return usuario_controller.auth_steam(contexto = {'request':request})
-
-def auth_steam_delete():
-    return usuario_controller.auth_steam_delete(contexto = {'request':request})
-
-def validar_usuario():
-    return usuario_controller.validar_usuario(contexto = {'request':request})
+from .controllers import usuario_controller as uc
+from .controllers import jogo_controller as jc
 
 def declarar_rotas(app):
-    app.add_url_rule("/cadastro", methods=["POST"], view_func=usuario_controller.criar_usuario)
+    app.add_url_rule("/cadastro", methods=["POST"], view_func=uc.cadastrar_usuario)
 
-    app.add_url_rule("/login", methods=["POST"], view_func=usuario_controller.logar_usuario)
+    app.add_url_rule("/login", methods=["POST"], view_func=uc.logar_usuario)
 
-    app.add_url_rule("/authSteam", methods=["POST"], view_func=auth_steam)
+    app.add_url_rule("/authSteam", methods=["POST"], view_func=uc.auth_steam)
 
     app.add_url_rule(
-      "/authSteam", methods=["DELETE"], view_func=auth_steam_delete
+      "/authSteam", methods=["DELETE"], view_func=uc.auth_steam_delete
     )
 
     app.add_url_rule(
-      "/esqueci_senha", methods=["POST"], view_func=validar_usuario
+      "/esqueci_senha", methods=["POST"], view_func=uc.validar_usuario
     )
 
     app.add_url_rule(
-      "/atualizar_senha", methods=["POST"], view_func=usuario_controller.atualizar_senha
+      "/atualizar_senha", methods=["POST"], view_func=uc.atualizar_senha
     )
 
     app.add_url_rule(
-      "/listar_jogos_steam", methods=["GET"], view_func=usuario_controller.listar_jogos_steam
+      "/listar_jogos_steam", methods=["GET"], view_func=jc.listar_jogos_steam
     )
---
+++ app/services/jogo_services.py
@@ -0,0 +1,24 @@
+import requests
+
+from ..views.jogo_view import serializar_jogos
+from ..utils import auth
+from .. import config
+
+@auth.token_required
+def listar_jogos_steam(contexto):
+    usuario = contexto["usuario"]
+    if not usuario.steam_id:
+        return {"erro": "nenhuma conta da steam associada a este usuario"}, 400
+    headers = {"x-webapi-key": config.STEAM_API_KEY}
+    parametros = {
+      "steamid": usuario.steam_id,
+      "include_appinfo": True,
+      "include_played_free_games": True
+    }
+    resposta = requests.get(
+      'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/',
+      headers=headers,
+      params=parametros,
+      timeout=15
+    )
+    return serializar_jogos(resposta.json()), 200
