1ff9dc0f9873230d123e9e57fcf6ce976b3e0f6a
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-20 21:47:39 -0300

    cria o script de pylint e pytest separados

--- .githooks/pre-push.sh
+++ .githooks/pre-push.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
 
-DISABLED_RULES=C0114,C0115,C0116,E1101,R0902
-DISABLED_TEST_RULES=W0621,W0212
-
-pytest --ignore=lib --ignore=lib64 && pylint $(git ls-files '*.py') --disable=$DISABLED_RULES,$DISABLED_TEST_RULES
+GIT_DIR=$(git rev-parse --show-toplevel)
+$GIT_DIR/scripts/pytest.sh && $GIT_DIR/scripts/pylint.sh
