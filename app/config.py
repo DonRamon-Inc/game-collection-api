8dc52276f4113475cc716e1f3aee1a4b15a8b90c
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-21 14:20:13 -0300

    adiciona um valor default para DATABASE_URL

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
+DATABASE_URL = os.getenv('DATABASE_URL', '').replace('postgres://', 'postgresql://')
 SECRET_KEY = os.getenv('SECRET_KEY')
 STEAM_API_KEY = os.getenv('STEAM_API_KEY')
 STEAM_API_URL = os.getenv('STEAM_API_URL')
