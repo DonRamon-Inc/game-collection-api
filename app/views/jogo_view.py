8b50b089f9a49137365e55a3abfd56eefc3c23c8
RamonRCM <costa.m.ramon@gmail.com>
2022-09-22 19:32:19 -0300

    serialização movida para view conforme padrão MVCS

--- app/views/jogo_view.py
+++ app/views/jogo_view.py
@@ -6,3 +6,16 @@ def serializar_jogos(resposta):
       "quantidade_jogos": quantidade_jogos,
       "jogos": jogos
     }
+
+def serializar_detalhes_jogo(resposta, jogo_id):
+
+    return {
+      "jogo_id":resposta[jogo_id]["data"]["steam_appid"],
+      "nome": resposta[jogo_id]["data"]["name"],
+      "descricao": resposta[jogo_id]["data"]["short_description"],
+      "capa": resposta[jogo_id]["data"]["header_image"],
+      "preco_atual": resposta[jogo_id]["data"]["price_overview"]["final_formatted"],
+      "preco_base": resposta[jogo_id]["data"]["price_overview"]["initial_formatted"],
+      "desconto": resposta[jogo_id]["data"]["price_overview"]["discount_percent"]
+    }
+    
