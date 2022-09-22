ee3cd425f2db1bf0a9108c5edab0ff6d6af63fc4
RamonRCM <costa.m.ramon@gmail.com>
2022-09-22 19:32:19 -0300

    serialização movida para view conforme padrão MVCS

--- app/services/jogo_services.py
+++ app/services/jogo_services.py
@@ -1,6 +1,6 @@
 import requests
 
-from ..views.jogo_view import serializar_jogos
+from ..views.jogo_view import serializar_jogos, serializar_detalhes_jogo
 from ..utils import auth
 from .. import config
 
@@ -26,7 +26,7 @@ def listar_jogos_steam(contexto):
 @auth.token_required
 def detalhes_jogo(contexto):
 
-    jogo_id = contexto["jogo_id"]
+    jogo_id = str(contexto["jogo_id"])
 
     parametros_request = {
       "appids": jogo_id,
@@ -37,15 +37,6 @@ def detalhes_jogo(contexto):
     resposta = requests.get(
       f"{config.STEAM_STORE_URL}/api/appdetails", params= parametros_request, timeout=15).json()
 
-    detalhes = {
-      "jogo_id":resposta[str(jogo_id)]["data"]["steam_appid"],
-      "nome": resposta[str(jogo_id)]["data"]["name"],
-      "descricao": resposta[str(jogo_id)]["data"]["short_description"],
-      "capa": resposta[str(jogo_id)]["data"]["header_image"],
-      "preco_atual": resposta[str(jogo_id)]["data"]["price_overview"]["final_formatted"],
-      "preco_base": resposta[str(jogo_id)]["data"]["price_overview"]["initial_formatted"],
-      "desconto": resposta[str(jogo_id)]["data"]["price_overview"]["discount_percent"]
-    }
 
-    return (detalhes, 200)
+    return (serializar_detalhes_jogo(resposta, jogo_id), 200)
     
