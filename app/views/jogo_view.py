ed05b723334c0285c2a5f7983c282e9923e065c3
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2022-09-26 20:07:51 -0300

    cria teste unitario para serializar_jogos

--- app/views/jogo_view.py
+++ app/views/jogo_view.py
@@ -1,8 +1,6 @@
 def serializar_jogos(resposta):
     quantidade_jogos = resposta["response"]["game_count"]
-    jogos = []
-    for game in resposta["response"]["games"]:
-        jogos.append(game["name"])
+    jogos = [jogo["name"] for jogo in resposta["response"]["games"]]
     jogos.sort()
     return {
       "quantidade_jogos": quantidade_jogos,
---
+++ app/views/jogo_view_test.py
@@ -0,0 +1,21 @@
+from . import jogo_view as jv
+
+def test_serializar_jogos_deve_ordernar_jogos_por_ordem_alfabetica():
+    jogos = [
+        {'name': 'Bioshock'},
+        {'name': 'Grand Theft Auto V'},
+        {'name': 'Crash Bandicoot'},
+        {'name': 'Battleblock Theather'}
+    ]
+    resposta = {'response': {'games': jogos, 'game_count': 4}}
+
+    resultado = jv.serializar_jogos(resposta)
+
+    assert resultado['quantidade_jogos'] == 4
+    jogos_esperados = [
+        'Battleblock Theather',
+        'Bioshock',
+        'Crash Bandicoot',
+        'Grand Theft Auto V'
+    ]
+    assert resultado['jogos'] == jogos_esperados
